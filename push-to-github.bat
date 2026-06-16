1adc6e438309c478fb31425e7098c51be0fdeee5
Kyron <kcumby2@gmail.com>
2026-06-15 22:37:31 -0400

    Add all site images: event-photos, athlete-photos, crew-photos

--- push-to-github.bat
+++ push-to-github.bat
@@ -4,7 +4,7 @@ echo  Pushing updates to GitHub...
 echo ============================================
 cd /d "%~dp0"
 git add .
-git commit -m "Update pricing: Team Package $499.99, Event Package $1,499.99"
+git commit -m "Add all site images: event-photos, athlete-photos, crew-photos"
 git push origin main
 echo.
 echo ============================================
