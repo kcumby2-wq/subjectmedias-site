f1962d49e7b562dbd2006f06c2fd4b7a131450f1
Kyron <kcumby2@gmail.com>
2026-06-15 21:59:08 -0400

    Update all pricing to .99 format

--- push-to-github.bat
+++ push-to-github.bat
@@ -1,19 +1,13 @@
 @echo off
 echo ============================================
-echo  Pushing Subject Medias site to GitHub...
+echo  Pushing updates to GitHub...
 echo ============================================
-
 cd /d "%~dp0"
-
-git init
 git add .
-git commit -m "Initial deploy: Subject Medias site"
-git branch -M main
-git remote add origin https://github.com/kcumby2-wq/subjectmedias-site.git
-git push -u origin main
-
+git commit -m "Update pricing: Team Package $499.99, Event Package $1,499.99"
+git push origin main
 echo.
 echo ============================================
-echo  Done! Check GitHub to confirm upload.
+echo  Done! Vercel will auto-deploy in ~30 sec.
 echo ============================================
 pause
